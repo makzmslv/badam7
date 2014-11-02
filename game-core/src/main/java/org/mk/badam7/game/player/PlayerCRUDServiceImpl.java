f35d348a11615f99851c3d2e7b17bcb364a9071a
Makarand Salvi <makarand.salvi@neptune-int.com>
2014-11-02 10:24:28 +0530

    added password encoder

--- game-core/src/main/java/org/mk/badam7/game/player/PlayerCRUDServiceImpl.java
+++ game-core/src/main/java/org/mk/badam7/game/player/PlayerCRUDServiceImpl.java
@@ -7,6 +7,7 @@ import org.mk.badam7.database.enums.UserAuthorities;
 import org.mk.badam7.gamecore.common.EmailSender;
 import org.mk.badam7.gamedto.player.PlayerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -29,9 +30,7 @@ public class PlayerCRUDServiceImpl implements PlayerCRUDService
             throw new IllegalArgumentException("dto cannot be null");
         }
 
-        Player player = dozerMapper.map(playerInDTO, Player.class);
-        player.setVerified(false);
-        player.setRole(UserAuthorities.NOT_VERIFIED.getRole());
+        Player player = createPlayerEntityFromDTO(playerInDTO);
         player = playerDAO.save(player);
         sendVerificationMail(player.getEmail());
         PlayerDTO playerDTO = dozerMapper.map(player, PlayerDTO.class);
@@ -39,8 +38,24 @@ public class PlayerCRUDServiceImpl implements PlayerCRUDService
         return playerDTO;
     }
 
+    private Player createPlayerEntityFromDTO(PlayerDTO playerInDTO)
+    {
+        Player player = dozerMapper.map(playerInDTO, Player.class);
+        player.setVerified(false);
+        String encodedPassword = encodeUserPassword(playerInDTO.getUsername(), playerInDTO.getPassword());
+        player.setPassword(encodedPassword);
+        player.setRole(UserAuthorities.NOT_VERIFIED.getRole());
+        return player;
+    }
+
     private void sendVerificationMail(String email)
     {
         emailSender.sendMail(email);
+    }
+
+    private String encodeUserPassword(String username, String password)
+    {
+        ShaPasswordEncoder encoder = new ShaPasswordEncoder();
+        return encoder.encodePassword(username, password);
     }
 }
